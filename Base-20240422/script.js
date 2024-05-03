a46acf272ca33348ebe9d93ac96323a0e89f67ca
Matheo Lopes <matheo.lopes@etu.unilim.fr>
2024-05-04 01:10:13 +0200

    changement fleche+animation

--- Base-20240422/script.js
+++ Base-20240422/script.js
@@ -8,30 +8,36 @@ function cacherSection(event){
     
     if (titre.getAttribute("value")==null || titre.getAttribute("value")==0){
         for (var i=1;i<enfant_section.length;i++){
-            
             enfant_section[i].style.display="none"
         }
         titre.setAttribute("value",1)
-        titre.innerHTML=titre.innerHTML.slice(0,titre.innerHTML.length-1)+"⭢"
-        //titre.innerHTML[titre.innerHTML.indexOf("⭣")]="⭢";
+        animationFleche=titre.lastChild.animate([{transform:"rotate(0deg)"}],{duration: 200})
+        animationFleche.onfinish=function(){
+            
+            titre.lastChild.style.transform="rotate(0deg)"
+        }
+        
         
         
     }
     else{
-        
+        titre.setAttribute("value",0)
         for (i=1;i<enfant_section.length;i++){
             
             enfant_section[i].style.display="contents"
         }
-        titre.setAttribute("value",0)
-        titre.innerHTML=titre.innerHTML.slice(0,titre.innerHTML.length-1)+"⭣"
+        animationFleche=titre.lastChild.animate([{transform:"rotate(90deg)"}],{duration: 200})
+        animationFleche.onfinish=function(){
+            
+            titre.lastChild.style.transform="rotate(90deg)"
+        }
     }
 }
 
 
 
 for (let i=0;i<titres.length;i++) {
-    titres[i].innerHTML+=" ⭣"
+    
     titres[i].addEventListener("click",cacherSection)
 }
 
