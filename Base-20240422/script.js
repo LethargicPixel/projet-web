bddd0175098f1014216d81f5539a47f19d4bc3ca
Matheo Lopes <matheo.lopes@etu.unilim.fr>
2024-05-06 16:20:57 +0200

    ex 4

--- Base-20240422/script.js
+++ Base-20240422/script.js
@@ -66,13 +66,13 @@ boutonsEnvoyer.addEventListener("click",function(event){
 /*-----------------question 3-----------------*/
 
 let liste_lien=[
-    "data/img/gallery/1_Limoges_Hotel_de_Ville.jpg",
-    "data/img/gallery/2_Limoges_Cour_du_Temple.jpg",
-    "data/img/gallery/3_Limoges_Gare_Benedictins.jpg",
-    "data/img/gallery/4_Limoges_Halles_Centrales.jpg",
-    "data/img/gallery/5_Limoges_Pont_St_Martial.jpg",
-    "data/img/gallery/6_Limoges_Rue_de_la_Boucherie.jpg",
-    "data/img/gallery/7_Cela_pourrait_etre_quoi.png"
+    "./data/img/gallery/1_Limoges_Hotel_de_Ville.jpg",
+    "./data/img/gallery/2_Limoges_Cour_du_Temple.jpg",
+    "./data/img/gallery/3_Limoges_Gare_Benedictins.jpg",
+    "./data/img/gallery/4_Limoges_Halles_Centrales.jpg",
+    "./data/img/gallery/5_Limoges_Pont_St_Martial.jpg",
+    "./data/img/gallery/6_Limoges_Rue_de_la_Boucherie.jpg",
+    "./data/img/gallery/7_Cela_pourrait_etre_quoi.png"
 ]
 
 let liste_legende=[
@@ -98,10 +98,11 @@ image_intro_suivante.setAttribute("id","image_intro_suivant")
 image_intro_suivante.style.clipPath="inset(0 100% 0 0)"
 div_image_intro.appendChild(image_intro_suivante)
 div_image_intro.style.height=getComputedStyle(image_intro).height
-div_image_intro.style.width=`${taille_image_intro}px`
+div_image_intro.style.width=`${taille_image_intro+100}px`
+div_image_intro.style.minWidth=`${taille_image_intro+100000}px`
 
 
-let duree_animation=500
+let duree_animation=300
 
 setInterval(function(){
     taille_image_intro=parseInt(getComputedStyle(image_intro).width)
@@ -120,7 +121,7 @@ setInterval(function(){
         compteur_image%=liste_lien.length
     }
 
-image_intro_suivante.firstElementChild.setAttribute("src",liste_lien[compteur_image])
+    image_intro_suivante.firstElementChild.setAttribute("src",liste_lien[compteur_image])
     image_intro_suivante.lastElementChild.innerHTML=liste_legende[compteur_image]
    
 
@@ -138,4 +139,46 @@ image_intro_suivante.firstElementChild.setAttribute("src",liste_lien[compteur_im
    
     
     
-},3000)
+},3000+duree_animation)
+
+/*-----------------question 4-----------------*/
+let body=document.querySelector("body")
+let collection_enfant_body=body.children
+let liste_enfant_body=convertisHTMLCollectionEnListe(collection_enfant_body)
+
+
+function convertisHTMLCollectionEnListe(collection){ 
+    var liste=[]
+    for (var i=0;i<collection.length;i++){
+        liste[i]=collection_enfant_body[i]
+    }
+    return liste
+}
+function melangeListe(liste_a_melanger){
+    var liste=liste_a_melanger.slice()
+    for (i=liste.length-1;i>0;i--){
+        
+        const j = Math.floor(Math.random() * (i + 1));
+        [liste[i],liste[j]]=[liste[j],liste[i]]
+    }
+    return liste.slice()
+}
+
+function melangeElementBody(event){
+
+    if(event.target.getAttribute("src")==liste_lien[liste_lien.length-1]){
+        var liste_enfant_body_melanger=melangeListe(liste_enfant_body)
+        var nouveau_body=document.createElement("body")
+
+        for(var i=0;i<liste_enfant_body_melanger.length;i++){
+        
+            nouveau_body.appendChild(liste_enfant_body_melanger[i])
+        }
+        body.remove()
+        document.querySelector("html").appendChild(nouveau_body)
+    
+    }
+    
+}
+image_intro.addEventListener("click",melangeElementBody)
+image_intro_suivante.addEventListener("click",melangeElementBody)
